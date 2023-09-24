ac9887c18bfaaad5b410e113fb72282d6e81932d
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2023-09-24 19:43:00 -0300

    basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './domain/app.service';
+import { AppController } from './driver/app.controller';
 
 @Module({
   imports: [],
--- src/driver/app.controller.ts
+++ src/driver/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from 'src/domain/app.service';
 
 @Controller()
 export class AppController {
