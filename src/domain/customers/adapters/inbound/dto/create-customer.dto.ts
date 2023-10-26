578b829272aacd7cf634f8f0c617a3589c3783ef
Hugo Marques <hugmsm@hotmail.com>
2023-10-26 17:50:32 -0300

    colocado API Property nos DTOs e ajustado docker compose para execução em Windows

--- src/domain/customers/adapters/inbound/dto/create-customer.dto.ts
+++ src/domain/customers/adapters/inbound/dto/create-customer.dto.ts
@@ -1,5 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCustomerDto {
+
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   cpf: string;
 }
