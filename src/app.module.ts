1999a1c7bbf67405b3cb66763f4cfe802c7b871b
Hiago <hiagofss98@gmail.com>
2024-01-13 13:21:53 -0300

    Set infrastructure folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { PostgresConfigServiceService } from './libs/databases/postgres.service';
+import { PostgresConfigServiceService } from './infrastructure/databases/postgres.service';
 
 import { AppService } from './domain/app.service';
 import { AppController } from './app.controller';
