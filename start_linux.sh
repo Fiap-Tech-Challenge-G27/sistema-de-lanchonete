26746b7a5bcca39bcb8f62886ab17333c43318cb
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-01-23 12:38:33 -0300

    ajustando script linux

--- start_linux.sh
+++ start_linux.sh
@@ -9,53 +9,30 @@ fi
 # Verificar se o arquivo existe
 arquivo="minikube"
 
-if [ -f "$arquivo" ]; then
+if command -v minikube &> /dev/null; then
     echo "Iniciando MiniKube."
-
     # Iniciar o MiniKube
-    ./minikube start --driver=docker
+    minikube start --driver=docker
     docker context use default
 else
     echo "O MiniKube não existe. Instalando..."
-
     # Baixar o instalador do MiniKube
-    url='https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64'
-    arquivo="minikube"
-    wget -O $arquivo $url
-
-    # Dar permissão de execução ao MiniKube
-    chmod +x $arquivo
-
-    # Mover o MiniKube para um diretório no PATH
-    sudo mv $arquivo /usr/local/bin/
-
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
     echo "MiniKube instalado com sucesso."
-
     echo "Iniciando MiniKube."
-
     # Iniciar o MiniKube
-    ./minikube start --driver=docker
+    minikube start --driver=docker
     docker context use default
 fi
 
 # Verificar se o kubectl está instalado
 if ! command -v kubectl &> /dev/null; then
     echo "O kubectl não está instalado. Instalando..."
-
     # Baixar o kubectl
-    kubectlUrl="https://dl.k8s.io/release/v1.29.0/bin/linux/amd64/kubectl"
-    arquivo="kubectl"
-
-    wget $kubectlUrl -O $arquivo
-
-    # Dar permissão de execução ao kubectl
-    chmod +x $arquivo
-
-    # Mover o kubectl para um diretório no PATH
-    sudo mv $arquivo /usr/local/bin/
-
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     echo "kubectl instalado com sucesso."
-
     echo "Testando kubectl."
     kubectl version --client
 fi
@@ -63,7 +40,7 @@ fi
 kubectl apply -f kubernetes/postgresql/postgresql_deployment.yml
 kubectl apply -f kubernetes/postgresql/postgresql_service.yml
 
-./minikube image build -t tech_challenge_fiap_4_group_27 .
+minikube image build -t tech_challenge_fiap_4_group_27 .
 
 kubectl apply -f kubernetes/backend/backend_deployment.yaml
 kubectl apply -f kubernetes/backend/backend_service.yml
@@ -71,4 +48,4 @@ kubectl apply -f kubernetes/backend/backend_service.yml
 echo "Aguardando inicialização da aplicação. (60 segundos)"
 sleep 60
 
-./minikube service backend
+minikube service backend
