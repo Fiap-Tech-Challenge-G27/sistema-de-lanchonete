18c0919d8532b6c3b91b40c66a4d23a30c682166
hugomsm <hugomaiadasilvamarques@gmail.com>
2024-03-09 15:08:22 -0300

    ajustando deployment

--- update_deployment.sh
+++ update_deployment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Substituir {{VERSION}} no arquivo deployment.yaml com a nova versão
-sed -i "s/{{VERSION}}/$1/g" deployment.yaml
+sed -i "s/{{VERSION}}/$1/g" kubernetes/backend/backend_deployment.yaml
