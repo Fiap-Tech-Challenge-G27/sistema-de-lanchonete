3dd6550c1a3edd848db2068862709339d554fe6e
KaioAmaralB <kaio411@hotmail.com>
2024-03-18 22:17:44 -0300

    Corrigindo url para conexão no banco

--- src/shared/infra/typeorm/postgres.service.ts
+++ src/shared/infra/typeorm/postgres.service.ts
@@ -11,12 +11,18 @@ import { OrdersProductsAmounts } from '@modules/orders/infra/typeorm/entities/or
 @Injectable()
 export class PostgresConfigServiceService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
+
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+ 
+    const dbConfigString = this.configService.get('TYPEORM_URL');
+
+    const dbConfig = JSON.parse(dbConfigString);
+
+    const typeormUrl = dbConfig.typeorm;
+
     return {
-      // type: this.configService.get('TYPEORM_CONNECTION'),
       type: "postgres",
-      url: "postgres://fe0x8xEtGHbkj8ME:yTDcAvztw8TW9ixB@postgresapp.c5m86oo0sf33.us-east-1.rds.amazonaws.com:5432/app",
-      // url: this.configService.get('TYPEORM_URL'),
+      url: typeormUrl,
       entities: [Customer, Product, Order, Category, OrdersProductsAmounts],
       synchronize: true,
       ssl: {
@@ -25,3 +31,31 @@ export class PostgresConfigServiceService implements TypeOrmOptionsFactory {
     };
   }
 }
+
+// import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
+// import { Customer } from '@modules/customers/infra/typeorm/entities/customer';
+// import { Product } from '@modules/products/infra/typeorm/entities/product';
+// import { Order } from '@modules/orders/infra/typeorm/entities/order';
+// import { Category } from '@modules/categories/infra/typeorm/entities/category';
+// import { OrdersProductsAmounts } from '@modules/orders/infra/typeorm/entities/orders-products-amounts';
+
+// @Injectable()
+// export class PostgresConfigServiceService implements TypeOrmOptionsFactory {
+//   constructor(private configService: ConfigService) {}
+//   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+//     return {
+//       // type: this.configService.get('TYPEORM_CONNECTION'),
+//       type: "postgres",
+//       url: this.configService.get('TYPEORM_URL'),
+//       // url: this.configService.get('TYPEORM_URL'),
+//       entities: [Customer, Product, Order, Category, OrdersProductsAmounts],
+//       synchronize: true,
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     };
+//   }
+// }
